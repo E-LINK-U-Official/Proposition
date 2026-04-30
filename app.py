6dfa64add693947852038af84e90feac8e986364
E-Link-U-Mission <278191463+E-LINK-U-Official@users.noreply.github.com>
2026-04-30 10:45:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ with st.sidebar:
     st.write("👤 **Architect:** Lia Ariadna Ruiz Ben")
     # Enlaces corregidos y funcionales
     st.write("🆔 **ORCID:** [0009-0006-2598-0625](https://orcid.org/0009-0006-2598-0625)")
-    st.write("🔗 **DOI:** [10.5281/zenodo.19876558](https://doi.org/10.5281/zenodo.19876558")
+    st.write("🔗 **DOI:** [10.5281/zenodo.19876558](//doi.org/10.5281/zenodo.19876558)")
     st.divider()
     m_t = "Methodology 2025" if lang == "English" else "Metodología 2025"
     st.markdown(f"**{m_t}**")
